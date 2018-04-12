9b6c94780f4a49b7be07d6dbc98602f0d172e6d7
Colby Williams <colbylwilliams@gmail.com>
2018-04-12 15:25:55 -0400

    initial code commit

---
+++ inventory/inventory/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Inventory
+//
+//  Created by Colby L Williams on 4/12/18.
+//  Copyright © 2018 Colby L Williams. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
